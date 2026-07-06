0d0a12d08851a1eba74f43289b7f91d58030ecb0
ASG <39720035+Akash-Sriram@users.noreply.github.com>
2026-07-06 13:43:06 +0530

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+  alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "de.mangelow.network"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "de.mangelow.network"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 4
+        versionName = "1.03"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+      compose = false
+      aidl = false
+      buildConfig = false
+      shaders = false
+    }
+
+    packaging {
+      resources {
+        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+      }
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+dependencies {
+  // This simple app only uses standard platform APIs, so we only need core-ktx
+  implementation(libs.androidx.core.ktx)
+}
